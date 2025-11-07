01fe7298b1718494979cee2653a411acbd939ab7
royzheng1106 <yor.gnehz@gmail.com>
2025-11-07 22:39:00 +0800

    update config

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,31 @@ import { CONFIG } from "./utils/config.js";
 const app = express();
 
 // Increase request size limit to handle base64 audio
-// app.use(express.json({ limit: "10mb" }));
-// app.use(express.urlencoded({ limit: "10mb", extended: true }));
-app.use(express.json({ limit: "25mb" }));
-app.use(express.urlencoded({ limit: "25mb", extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 // Middleware for API key
 function requireApiKey(req: express.Request, res: express.Response, next: express.NextFunction) {
-  // let apiKey = "";
+  let apiKey = "";
 
-  // let authHeader = Array.isArray(req.headers['authorization'])
-  //   ? req.headers['authorization'][0]
-  //   : req.headers['authorization'];
+  let authHeader = Array.isArray(req.headers['authorization'])
+    ? req.headers['authorization'][0]
+    : req.headers['authorization'];
 
-  // if (authHeader && authHeader.startsWith('Bearer ')) {
-  //   apiKey = authHeader.substring(7); // strip 'Bearer '
-  // }
-  // if (apiKey !== CONFIG.API_KEY) {
-  //   return res.status(401).json({ ok: false, error: 'Unauthorized: invalid API key' });
-  // }
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    apiKey = authHeader.substring(7); // strip 'Bearer '
+  }
+  if (apiKey !== CONFIG.API_KEY) {
+    return res.status(401).json({ ok: false, error: 'Unauthorized: invalid API key' });
+  }
   next();
 }
 
 function requireTelegramSecret(req: express.Request, res: express.Response, next: express.NextFunction) {
-  // const apiKey = req.headers['x-telegram-bot-api-secret-token'];
-  // console.log(apiKey);
-  // console.log(CONFIG.TELEGRAM_SECRET);
-  // if (apiKey !== CONFIG.TELEGRAM_SECRET) return res.status(403).send("Forbidden");
+  const apiKey = req.headers['x-telegram-bot-api-secret-token'];
+  console.log(apiKey);
+  console.log(CONFIG.TELEGRAM_SECRET);
+  if (apiKey !== CONFIG.TELEGRAM_SECRET) return res.status(403).send("Forbidden");
   next();
 }
 
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -42,8 +42,8 @@ export const CONFIG = {
   PORT: process.env.PORT || 3000,
   TELEGRAM_BOT_TOKEN: requireEnv("TELEGRAM_BOT_TOKEN"),
   AGENTS_API_KEY: requireEnv("AGENTS_API_KEY"),
-  // TELEGRAM_SECRET: requireEnv("TELEGRAM_SECRET"),
-  // API_KEY: requireEnv("API_KEY"),
+  TELEGRAM_SECRET: requireEnv("TELEGRAM_SECRET"),
+  API_KEY: requireEnv("API_KEY"),
   // BASE_URL: requireEnv("BASE_URL")
     // ? `https://${process.env.BASE_URL}`
     // : process.env.VERCEL_URL
